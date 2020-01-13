1a7da9618543a62a7a4d38b87e335028ae851c3d
hare.mahto <harekrishnamahto18@gmail.com>
2020-01-13 15:36:51 +0530

    color changed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Todos from './Todos'
+import Background from './background.jpeg'
+
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{"backgroundColor":"#be8abf","marginLeft":"300px","marginRight":"300px","borderRadius":"15px"}}>
       <h1>Welcome to TodoApp :)</h1>
       <Todos />
     </div>
