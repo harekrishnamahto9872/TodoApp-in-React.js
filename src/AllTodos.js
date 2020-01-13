d6765af84cff80830371a62ca851ca6ae65c445e
hare.mahto <harekrishnamahto18@gmail.com>
2020-01-13 13:51:53 +0530

    items align center

--- src/AllTodos.js
+++ src/AllTodos.js
@@ -3,7 +3,7 @@ import React from 'react'
 function AllTodos({todos, deleteTodoItem}){
     const todosList = todos.map(todo =>{
         return(
-            <div className="todo" key={todo.id}>
+            <div className="todo" key={todo.id} style={{"textAlign":"center"}}>
                 <h2>{todo.content} <button onClick={() =>{deleteTodoItem(todo.id)}}>Delete</button></h2>
             </div>
         )
